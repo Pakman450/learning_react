1feaa6a6399d93e371aa6dade4347e809c782562
Steven Pak <steven.pak10@gmail.com>
2023-03-26 21:59:49 -0400

    feat: learning set state

--- my-project/src/components/Message.jsx
+++ my-project/src/components/Message.jsx
@@ -23,9 +23,66 @@ class Message extends Component{
     //this works because you aren't mutating the counter. you are just getting the previous value and reassigning the counter 
     // value. 
     changeCounter(){
+        // //this would change the counter value after it 
+        // //console logs the counter value
+        // this.setState({
+        //     counter: this.state.counter + 1
+        // })
+        // console.log(this.state.counter)
+
+
+        //Here if you want to console log AFTER the counter value 
+        //changes... You must add a call back function as a function parameter in the 
+        //this set state method.
         this.setState({
             counter: this.state.counter + 1
-        })
+        },
+        //Whenever you want to execute code that shows the value AFTER the changed states
+        //of the counter.. do this..   
+            () => {
+                console.log("callback func"+this.state.counter)
+            }
+        )
+
+        //this is asynchronous 
+        console.log(this.state.counter)
+    }
+
+
+    //This is where you ACUTALLY increment fvie
+    //you have to pass in a function and not an object like shown above. 
+    //If you want to edit th previous state and you want to
+    //call this function multiple times. 
+    //Do it this way.  
+    actuallyIncrementFive(){
+        this.setState((prevState)=>({
+            counter: prevState.counter +1
+        }))
+    }
+
+    
+    incrementFive(){
+
+        //This doesn't actuallty increment five times
+        //because it combines these method calls into one
+        //for better perforamnce. React actually does this. 
+        //not the javascript program.
+        //this.changeCounter()
+        //this.changeCounter()
+        //this.changeCounter()
+        //this.changeCounter()
+        //this.changeCounter()
+
+        this.actuallyIncrementFive()
+        this.actuallyIncrementFive()
+        this.actuallyIncrementFive()
+        this.actuallyIncrementFive()
+        this.actuallyIncrementFive()
+
+
+
+
+  
     }
     render(){
         return( 
@@ -34,6 +91,7 @@ class Message extends Component{
                 <button onClick={() => {this.changeMessage()}}>Please subscribe</button>
                 <h1> this is {this.state.counter}</h1>
                 <button onClick={() => {this.changeCounter()}}>Please counter</button>
+                <button onClick={() => {this.incrementFive()}}>Increment five</button>
 
             </div>
 
